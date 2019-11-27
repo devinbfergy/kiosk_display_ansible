4104403c0f19f8f9aa78436efe9ba85cfb58bfcd
devinbfergy <devin.ferguson.it@gmail.com>
2019-11-27 14:26:58 -0600

    updated push of lots of changes

--- roles/setup_kiosk/templates/kiosk.sh
+++ roles/setup_kiosk/templates/kiosk.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 # run this in display 0 aka the monitor
-export DISPLAY=:0
+export DISPLAY=:0.0
 xset -dpms
 xset s off
 xset s noblank
 
+# turn off certain hot keys that allow one to escape chromium/open terminal etc
+xmodmap -e "keycode 37 = "
+xmodmap -e "keycode 105 = "
+xmodmap -e "keycode 133 = "
+xmodmap -e "keycode 134 = "
+xmodmap -e "keycode 67 = "
+
+# make the mouse hidden
 unclutter &
 
 chromium-browser --user-data-dir=/home/{{autostart_user}}/screen1 --kiosk --window-position=0,0 --app="{{url1}}" &
